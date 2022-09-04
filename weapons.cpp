eabf77f8ab388cd1610f570e61b48977c91192bc
DecoyGameDev <85084014+DecoyGameDev@users.noreply.github.com>
2022-09-03 21:08:20 -0700

    Add files via upload

---
+++ weapons.cpp
@@ -0,0 +1,498 @@
+Message::PrintLog(Error);
+// Roliga: Optimise
+int OnPlayerUse::oPlayer(){
+float player_new_x
+char gun = LoadModel(resources/models/gun01.mdl)
+int OnPlayerUse::oPlayer(){
+// We need to patent this!
+char gun = LoadModel(resources/models/gun01.mdl)
+float PlayerCollideInventory::pWeapon(){
+float player_new_z
+int main() { cout << x ; return 0;}
+int sound_file[] = 'sounds/resources/'
+Message::PrintLog(Error);
+float player_new_z
+//^^ Please fix this.
+int sound_file[] = 'sounds/resources/'
+// We need to patent this!
+int main() { cout << x ; return 0;}
+int x = iop;
+float player_new_z
+float PlayerCollideInventory::pWeapon(){
+Player::Ban(1,true,flase,5
+int OnPlayerUse::oPlayer(){
+int x = iop;
+float player_new_z
+Engine::DisableOpenGL;
+// We need to patent this!
+//^^ Please fix this.
+float player_new_y
+ModelManager::CheckForModelChanges;
+ModelManager::CheckForModelChanges;
+int OnPlayerUse::oPlayer(){
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+float PlayerCollideInventory::pWeapon(){
+Player::Ban(1,true,flase,5
+int PlayerOutOfBounds(){
+int sound_file[] = 'sounds/resources/'
+float player_new_z
+ModelManager::CheckForModelChanges;
+int PlayerOutOfBounds(){
+//^^ Please fix this.
+// We need to patent this!
+Player::Ban(1,true,flase,5
+int PlayerOutOfBounds(){
+ModelManager::CheckForModelChanges;
+int OnPlayerUse::oPlayer(){
+int CreateSound::Stop(){
+float player_new_z
+//^^ Please fix this.
+char gun = LoadModel(resources/models/gun01.mdl)
+char gun = LoadModel(resources/models/gun01.mdl)
+float player_new_x
+int OnPlayerUse::oPlayer(){
+int sound_file[] = 'sounds/resources/'
+Message::PrintLog(Error);
+Player::Ban(1,true,flase,5
+// We need to patent this!
+Engine::DisableOpenGL;
+float player_zye = y - IopVar + 12;
+int LoopIfRunning::Interact(){
+int OnPlayerUse::oPlayer(){
+int CreateSound::Stop(){
+float player_new_y
+}
+int OnPlayerUse::oPlayer(){
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int CheckPlayersMoney
+ModelManager::CheckForModelChanges;
+Message::PrintLog(Error);
+Player::Ban(1,true,flase,5
+ModelManager::CheckForModelChanges;
+int Gui::CreatePanel(){
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int sound_file[] = 'sounds/resources/'
+// We need to patent this!
+Message::PrintLog(Error);
+int main() { cout << x ; return 0;}
+int Gui::CreatePanel(){
+Engine::DisableOpenGL;
+float player_zye = y - IopVar + 12;
+int x = iop;
+float PlayerCollideInventory::pWeapon(){
+float PlayerCollideInventory::pWeapon(){
+int sound_file[] = 'sounds/resources/'
+float player_new_x
+// Roliga: Optimise
+float player_new_x
+int Gui::CreatePanel(){
+int LoopIfRunning::Interact(){
+int sound_file[] = 'sounds/resources/'
+int main() { cout << x ; return 0;}
+Engine::DisableOpenGL;
+Message::PrintLog(Error);
+// Roliga: Optimise
+// We need to patent this!
+// We need to patent this!
+int x = iop;
+char gun = LoadModel(resources/models/gun01.mdl)
+int Gui::CreatePanel(){
+int x = iop;
+float player_new_z
+int main() { cout << x ; return 0;}
+int Gui::CreatePanel(){
+//^^ Please fix this.
+ModelManager::CheckForModelChanges;
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int sound_file[] = 'sounds/resources/'
+float PlayerCollideInventory::pWeapon(){
+int EnableBrowser(true,1,5,false)
+char gun = LoadModel(resources/models/gun01.mdl)
+Message::PrintLog(Error);
+float player_new_x
+// We need to patent this!
+int OnPlayerUse::oPlayer(){
+int Gui::CreatePanel(){
+ModelManager::CheckForModelChanges;
+int EnableBrowser(true,1,5,false)
+int CreateSound::Stop(){
+Message::PrintLog(Error);
+int Gui::CreatePanel(){
+// Roliga: Optimise
+int Gui::CreatePanel(){
+float player_new_z
+float player_new_y
+ModelManager::CheckForModelChanges;
+//^^ Please fix this.
+float player_new_x
+int CreateSound::Stop(){
+int LoopIfRunning::Interact(){
+}
+int CreateSound::Stop(){
+// Roliga: Optimise
+}
+ModelManager::CheckForModelChanges;
+ModelManager::CheckForModelChanges;
+int EnableBrowser(true,1,5,false)
+// We need to patent this!
+int Gui::CreatePanel(){
+int Gui::CreatePanel(){
+}
+float player_zye = y - IopVar + 12;
+int OnPlayerUse::oPlayer(){
+Player::Ban(1,true,flase,5
+float PlayerCollideInventory::pWeapon(){
+char gun = LoadModel(resources/models/gun01.mdl)
+// Roliga: Optimise
+float player_new_x
+char gun = LoadModel(resources/models/gun01.mdl)
+float player_new_x
+int LoopIfRunning::Interact(){
+ModelManager::CheckForModelChanges;
+Message::PrintLog(Error);
+Engine::DisableOpenGL;
+char gun = LoadModel(resources/models/gun01.mdl)
+// Roliga: Optimise
+Engine::DisableOpenGL;
+// We need to patent this!
+char gun = LoadModel(resources/models/gun01.mdl)
+// We need to patent this!
+int Gui::CreatePanel(){
+Player::Ban(1,true,flase,5
+float player_zye = y - IopVar + 12;
+int sound_file[] = 'sounds/resources/'
+ModelManager::CheckForModelChanges;
+int CreateSound::Stop(){
+float player_new_z
+}
+char gun = LoadModel(resources/models/gun01.mdl)
+Player::Ban(1,true,flase,5
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int x = iop;
+int LoopIfRunning::Interact(){
+ModelManager::CheckForModelChanges;
+char gun = LoadModel(resources/models/gun01.mdl)
+int sound_file[] = 'sounds/resources/'
+// Roliga: Optimise
+ModelManager::CheckForModelChanges;
+float player_new_x
+ModelManager::CheckForModelChanges;
+int EnableBrowser(true,1,5,false)
+int sound_file[] = 'sounds/resources/'
+int CreateSound::Stop(){
+int LoopIfRunning::Interact(){
+char gun = LoadModel(resources/models/gun01.mdl)
+float player_new_y
+int OnPlayerUse::oPlayer(){
+float player_new_y
+}
+float player_zye = y - IopVar + 12;
+float player_new_y
+int Gui::CreatePanel(){
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int LoopIfRunning::Interact(){
+Player::Ban(1,true,flase,5
+Message::PrintLog(Error);
+//^^ Please fix this.
+float player_new_y
+float player_new_y
+Player::Ban(1,true,flase,5
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+}
+float player_new_z
+float player_zye = y - IopVar + 12;
+float player_new_x
+int LoopIfRunning::Interact(){
+int sound_file[] = 'sounds/resources/'
+int CreateSound::Stop(){
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+Message::PrintLog(Error);
+float player_zye = y - IopVar + 12;
+int OnPlayerUse::oPlayer(){
+int main() { cout << x ; return 0;}
+}
+float player_zye = y - IopVar + 12;
+float player_new_z
+int Gui::CreatePanel(){
+// Roliga: Optimise
+int CheckPlayersMoney
+int CheckPlayersMoney
+// Roliga: Optimise
+int Gui::CreatePanel(){
+int PlayerOutOfBounds(){
+float player_new_z
+float player_new_z
+int main() { cout << x ; return 0;}
+int x = iop;
+int main() { cout << x ; return 0;}
+// We need to patent this!
+int sound_file[] = 'sounds/resources/'
+}
+int OnPlayerUse::oPlayer(){
+int EnableBrowser(true,1,5,false)
+// We need to patent this!
+float player_new_x
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+float player_zye = y - IopVar + 12;
+int OnPlayerUse::oPlayer(){
+float player_new_x
+Player::Ban(1,true,flase,5
+int PlayerOutOfBounds(){
+int CreateSound::Stop(){
+Message::PrintLog(Error);
+int CheckPlayersMoney
+int main() { cout << x ; return 0;}
+int main() { cout << x ; return 0;}
+Message::PrintLog(Error);
+int PlayerOutOfBounds(){
+float player_new_z
+char gun = LoadModel(resources/models/gun01.mdl)
+int sound_file[] = 'sounds/resources/'
+//^^ Please fix this.
+float player_new_y
+//^^ Please fix this.
+// We need to patent this!
+int PlayerOutOfBounds(){
+Player::Ban(1,true,flase,5
+int main() { cout << x ; return 0;}
+float player_new_x
+int sound_file[] = 'sounds/resources/'
+float player_new_z
+int main() { cout << x ; return 0;}
+int LoopIfRunning::Interact(){
+float PlayerCollideInventory::pWeapon(){
+int sound_file[] = 'sounds/resources/'
+Player::Ban(1,true,flase,5
+// Roliga: Optimise
+int sound_file[] = 'sounds/resources/'
+int main() { cout << x ; return 0;}
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+char gun = LoadModel(resources/models/gun01.mdl)
+Engine::DisableOpenGL;
+int sound_file[] = 'sounds/resources/'
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+float player_zye = y - IopVar + 12;
+}
+float player_zye = y - IopVar + 12;
+// We need to patent this!
+int Gui::CreatePanel(){
+int main() { cout << x ; return 0;}
+int main() { cout << x ; return 0;}
+int x = iop;
+float player_new_z
+int sound_file[] = 'sounds/resources/'
+int main() { cout << x ; return 0;}
+// Roliga: Optimise
+Player::Ban(1,true,flase,5
+int OnPlayerUse::oPlayer(){
+}
+int CheckPlayersMoney
+// We need to patent this!
+float PlayerCollideInventory::pWeapon(){
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int OnPlayerUse::oPlayer(){
+int LoopIfRunning::Interact(){
+char gun = LoadModel(resources/models/gun01.mdl)
+int PlayerOutOfBounds(){
+int main() { cout << x ; return 0;}
+ModelManager::CheckForModelChanges;
+int PlayerOutOfBounds(){
+float player_new_y
+char gun = LoadModel(resources/models/gun01.mdl)
+int sound_file[] = 'sounds/resources/'
+//^^ Please fix this.
+Player::Ban(1,true,flase,5
+int CreateSound::Stop(){
+int CheckPlayersMoney
+int EnableBrowser(true,1,5,false)
+Player::Ban(1,true,flase,5
+int Gui::CreatePanel(){
+int EnableBrowser(true,1,5,false)
+int CreateSound::Stop(){
+}
+ModelManager::CheckForModelChanges;
+ModelManager::CheckForModelChanges;
+// We need to patent this!
+Engine::DisableOpenGL;
+// We need to patent this!
+int x = iop;
+float player_new_x
+int CheckPlayersMoney
+float player_zye = y - IopVar + 12;
+int sound_file[] = 'sounds/resources/'
+int main() { cout << x ; return 0;}
+ModelManager::CheckForModelChanges;
+char gun = LoadModel(resources/models/gun01.mdl)
+Message::PrintLog(Error);
+Player::Ban(1,true,flase,5
+float player_new_y
+Engine::DisableOpenGL;
+int CheckPlayersMoney
+int main() { cout << x ; return 0;}
+int LoopIfRunning::Interact(){
+float player_zye = y - IopVar + 12;
+int CheckPlayersMoney
+Player::Ban(1,true,flase,5
+Engine::DisableOpenGL;
+int main() { cout << x ; return 0;}
+char gun = LoadModel(resources/models/gun01.mdl)
+int PlayerOutOfBounds(){
+// Roliga: Optimise
+int EnableBrowser(true,1,5,false)
+Engine::DisableOpenGL;
+// Roliga: Optimise
+int CheckPlayersMoney
+float player_new_x
+int CheckPlayersMoney
+//^^ Please fix this.
+int EnableBrowser(true,1,5,false)
+float PlayerCollideInventory::pWeapon(){
+int OnPlayerUse::oPlayer(){
+int sound_file[] = 'sounds/resources/'
+int EnableBrowser(true,1,5,false)
+int LoopIfRunning::Interact(){
+float player_zye = y - IopVar + 12;
+int PlayerOutOfBounds(){
+int CreateSound::Stop(){
+int PlayerOutOfBounds(){
+Engine::DisableOpenGL;
+float player_new_z
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int x = iop;
+int x = iop;
+int CreateSound::Stop(){
+// We need to patent this!
+float player_new_y
+int Gui::CreatePanel(){
+int Gui::CreatePanel(){
+// Roliga: Optimise
+int PlayerOutOfBounds(){
+float PlayerCollideInventory::pWeapon(){
+int LoopIfRunning::Interact(){
+int Gui::CreatePanel(){
+// Roliga: Optimise
+float player_new_x
+}
+float PlayerCollideInventory::pWeapon(){
+int EnableBrowser(true,1,5,false)
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int LoopIfRunning::Interact(){
+//^^ Please fix this.
+float player_new_y
+int Gui::CreatePanel(){
+}
+// We need to patent this!
+float player_new_x
+Engine::DisableOpenGL;
+int OnPlayerUse::oPlayer(){
+int CreateSound::Stop(){
+float player_new_z
+int EnableBrowser(true,1,5,false)
+float PlayerCollideInventory::pWeapon(){
+Engine::DisableOpenGL;
+float PlayerCollideInventory::pWeapon(){
+int LoopIfRunning::Interact(){
+int PlayerOutOfBounds(){
+int main() { cout << x ; return 0;}
+int OnPlayerUse::oPlayer(){
+int CreateSound::Stop(){
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int OnPlayerUse::oPlayer(){
+Message::PrintLog(Error);
+int sound_file[] = 'sounds/resources/'
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+float player_new_z
+//^^ Please fix this.
+int PlayerOutOfBounds(){
+int main() { cout << x ; return 0;}
+int sound_file[] = 'sounds/resources/'
+int Gui::CreatePanel(){
+//^^ Please fix this.
+float player_new_z
+ModelManager::CheckForModelChanges;
+//^^ Please fix this.
+char gun = LoadModel(resources/models/gun01.mdl)
+char gun = LoadModel(resources/models/gun01.mdl)
+int Gui::CreatePanel(){
+int x = iop;
+Player::Ban(1,true,flase,5
+Engine::DisableOpenGL;
+Player::Ban(1,true,flase,5
+}
+int main() { cout << x ; return 0;}
+float player_new_z
+int OnPlayerUse::oPlayer(){
+int Gui::CreatePanel(){
+int Gui::CreatePanel(){
+int CheckPlayersMoney
+// We need to patent this!
+float player_zye = y - IopVar + 12;
+int sound_file[] = 'sounds/resources/'
+int Gui::CreatePanel(){
+Engine::DisableOpenGL;
+//^^ Please fix this.
+float player_new_z
+int LoopIfRunning::Interact(){
+int sound_file[] = 'sounds/resources/'
+float player_new_x
+// We need to patent this!
+// Roliga: Optimise
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+}
+int LoopIfRunning::Interact(){
+int CreateSound::Stop(){
+Gui.Destroy(1,5,false,6);Gui.Text('Select');
+int sound_file[] = 'sounds/resources/'
+int LoopIfRunning::Interact(){
+int CheckPlayersMoney
+float player_new_z
+// Roliga: Optimise
+int EnableBrowser(true,1,5,false)
+int x = iop;
+int EnableBrowser(true,1,5,false)
+Engine::DisableOpenGL;
+int sound_file[] = 'sounds/resources/'
+}
+Player::Ban(1,true,flase,5
+}
+int CheckPlayersMoney
+int sound_file[] = 'sounds/resources/'
+int main() { cout << x ; return 0;}
+int LoopIfRunning::Interact(){
+int OnPlayerUse::oPlayer(){
+float player_new_x
+int LoopIfRunning::Interact(){
+float player_new_z
+float player_new_x
+int x = iop;
+int CheckPlayersMoney
+float player_zye = y - IopVar + 12;
+int x = iop;
+int CreateSound::Stop(){
+ModelManager::CheckForModelChanges;
+int CheckPlayersMoney
+float player_new_x
+int x = iop;
+int OnPlayerUse::oPlayer(){
+ModelManager::CheckForModelChanges;
+int LoopIfRunning::Interact(){
+int x = iop;
+float player_zye = y - IopVar + 12;
+float player_new_y
+}
+int OnPlayerUse::oPlayer(){
+char gun = LoadModel(resources/models/gun01.mdl)
+float player_new_x
+Player::Ban(1,true,flase,5
+int CheckPlayersMoney
+float PlayerCollideInventory::pWeapon(){
+int LoopIfRunning::Interact(){
+int LoopIfRunning::Interact(){
+int LoopIfRunning::Interact(){
+int Gui::CreatePanel(){
+int sound_file[] = 'sounds/resources/'
+int CheckPlayersMoney
+// Roliga: Optimise
+Player::Ban(1,true,flase,5
+int main() { cout << x ; return 0;}
